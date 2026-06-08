4b9c4d103025acf8586b4838b6519d0d1ac59f14
InvisibleSpiders <132212539+InvisibleSpiders@users.noreply.github.com>
2026-06-07 21:30:59 -0600

    Add HavenCore API dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,22 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://repo.codemc.io/repository/maven-public/")
+        maven("https://maven.pkg.github.com/InvisibleSpiders/HavenCore") {
+            credentials {
+                username = providers.gradleProperty("gpr.user")
+                    .orElse(providers.environmentVariable("GITHUB_ACTOR"))
+                    .orElse("InvisibleSpiders")
+                    .get()
+                password = providers.gradleProperty("gpr.key")
+                    .orElse(providers.environmentVariable("GITHUB_TOKEN"))
+                    .orElse("")
+                    .get()
+            }
+        }
     }
 }
 
